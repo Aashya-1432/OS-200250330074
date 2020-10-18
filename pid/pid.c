c36b745330d26867818db0521adc466c102cf868
Aashya1432 <aashutoshpanchgalle1@gmail.com>
2020-10-18 10:56:42 +0530

    process id how to look at and these is tree type of structure pit and ppid and in that use man file to acess the pid api and sume information you can acess the api then 1st u take the Number and api check info. Also try the prog. check the using getpid

---
+++ pid/pid.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+#include"getpid.h"
+
+int main()
+{
+    pid_t id;
+    pid_t parent_id;
+
+    id = getpid();
+    parent_id = getppid();
+
+    printf("PID : %d \n",id);
+    printf("PPID : %d \n",parent_id);
+
+    return 0;
+}
